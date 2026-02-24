0518a80ee981f846696dd0697f5fd8afa352f638
vicentt16 <vicentecvarguez@gmail.com>
2026-02-24 12:36:36 -0600

    agregar api a router

--- src/repo_api_equipo_e/routers/Prestashop/prestashop.py
+++ src/repo_api_equipo_e/routers/Prestashop/prestashop.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from . import productSku, orderReference
+from . import productSku, orderReference, customers
 
 router = APIRouter(prefix="/prestashop", tags=["PrestaShop"])
 
 router.include_router(productSku.router)
 router.include_router(orderReference.router)
+router.include_router(customers.router)
