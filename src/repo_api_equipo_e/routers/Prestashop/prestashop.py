55f36b612e4cd9297e15067df33a2ac4d0373600
Abraham089 <abrahamgb274@gmail.com>
2026-02-24 12:29:24 -0600

    Ordenes

--- src/repo_api_equipo_e/routers/Prestashop/prestashop.py
+++ src/repo_api_equipo_e/routers/Prestashop/prestashop.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from . import productSku, orderReference
+from . import productSku, orderReference, order
 
 router = APIRouter(prefix="/prestashop", tags=["PrestaShop"])
 
 router.include_router(productSku.router)
 router.include_router(orderReference.router)
+router.include_router(order.router)
