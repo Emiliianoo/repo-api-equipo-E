62a428fe5dfc765239793cc6c419e50be5945af2
Emiliano <133832748+Emiliianoo@users.noreply.github.com>
2026-02-23 22:15:47 -0600

    Agregar router de órdenes por referencia PrestaShop

--- src/repo_api_equipo_e/routers/Prestashop/prestashop.py
+++ src/repo_api_equipo_e/routers/Prestashop/prestashop.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import productSku
+from . import productSku, orderReference
 
 router = APIRouter(prefix="/prestashop", tags=["PrestaShop"])
 
 router.include_router(productSku.router)
+router.include_router(orderReference.router)
