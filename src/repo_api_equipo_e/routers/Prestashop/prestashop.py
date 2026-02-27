26cdadc556f1e7d1bc57e54122d365057b3916a2
Abraham089 <abrahamgb274@gmail.com>
2026-02-27 12:06:51 -0600

    Endpoint funcional y probado

--- src/repo_api_equipo_e/routers/Prestashop/prestashop.py
+++ src/repo_api_equipo_e/routers/Prestashop/prestashop.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from . import productSku, orderReference, customers, suppliers, payments, orders, products, bulkCreateFromOdoo
+from . import productSku, orderReference, customers, suppliers, payments, orders, products, bulkCreateFromOdoo, referenceCreateFromOdoo
 
 router = APIRouter(prefix="/prestashop", tags=["PrestaShop"])
 
@@ -11,3 +11,4 @@ router.include_router(payments.router)
 router.include_router(products.router)
 router.include_router(orders.router)
 router.include_router(bulkCreateFromOdoo.router)
+router.include_router(referenceCreateFromOdoo.router)
