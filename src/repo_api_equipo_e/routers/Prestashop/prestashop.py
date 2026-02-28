2f0e768b5f6fa2a43c43534cde89417fd2d1b733
Arturo7baldipro <153790614+Arturo7baldipro@users.noreply.github.com>
2026-02-27 19:07:45 -0600

    Desactivar producto de prestashop

--- src/repo_api_equipo_e/routers/Prestashop/prestashop.py
+++ src/repo_api_equipo_e/routers/Prestashop/prestashop.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from . import productSku, orderReference, customers, suppliers, payments, orders, products, bulkCreateFromOdoo
+from . import orderReference, customers, suppliers, payments, orders, products, bulkCreateFromOdoo, productDeactivate
 
 router = APIRouter(prefix="/prestashop", tags=["PrestaShop"])
 
-router.include_router(productSku.router)
 router.include_router(orderReference.router)
 router.include_router(customers.router)
 router.include_router(suppliers.router)
@@ -11,3 +10,4 @@ router.include_router(payments.router)
 router.include_router(products.router)
 router.include_router(orders.router)
 router.include_router(bulkCreateFromOdoo.router)
+router.include_router(productDeactivate.router)
